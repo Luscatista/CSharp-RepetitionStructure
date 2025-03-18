f5c0bd3e1a2748060881f9684d8fa37d926efb78
Lucas <lucas.baptista@live.com>
2025-03-17 21:54:16 -0300

    Exercicios com estruturas de repeticao

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿Console.WriteLine("1. Contagem de 1 a 10");
+Console.WriteLine("Crie um programa que exiba os números de 1 a 20 na tela.");
+Console.WriteLine();
+
+for(int i = 1; i <= 20; i++)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine();
+
+Console.WriteLine("2. Validação de senha");
+Console.WriteLine("Peça ao usuário para inserir a senha \"1234\". Continue pedindo até que ele acerte.");
+Console.WriteLine();
+
+int senha;
+do
+{
+    Console.WriteLine("Digite a senha");
+    senha = int.Parse(Console.ReadLine());
+} while (senha != 1234);
+Console.WriteLine();
+
+Console.WriteLine("3. Tabuada de um número");
+Console.WriteLine("Peça ao usuário um número e exiba a tabuada dele de 1 a 10");
+Console.WriteLine();
+
+Console.WriteLine("Escolha um numero para exibir sua tabuada:");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{numero} * {i} é igual a : {numero * i}");
+}
--- obj/Debug/net9.0/Estruturas de Repeticao.AssemblyInfo.cs
+++ obj/Debug/net9.0/Estruturas de Repeticao.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estruturas de Repeticao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97906cb468983852ef9b93293b39efd1cfa91bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estruturas de Repeticao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estruturas de Repeticao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
